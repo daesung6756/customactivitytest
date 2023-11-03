735635c464ad0a4eb135256d879b95029401fbce
daesung.lee <leeds019@naver.com>
2023-11-03 18:00:02 +0900

    일부 디자인 수정

--- customActivity.js
+++ customActivity.js
@@ -4,11 +4,10 @@ connection.trigger("ready")
 
 connection.on("initActivity", () => {
     console.log("클릭했다.")
-    alert("커스텀엑티비틔를 설정하세요.")
 })
 
 connection.on("clickedNext", () => {
     console.log("완료했다.")
-    var configuration = alert("저장 되었습니다.")
+    var configuration;
     connection.trigger('updateActivity', configuration)
 })
