e595a12a393ceda4459be75f30fb118f88ece894
daesung.lee <leeds019@naver.com>
2023-11-03 14:42:31 +0900

    sdsd

--- customActivity.js
+++ customActivity.js
@@ -2,13 +2,13 @@ var connection = new Postmonger.Session();
 
 connection.trigger("ready")
 
-connection.on("initActivity", (data) => {
-    console.log("클릭했다..")
-    document.getElementById('configuration').value = JSON.stringify(data , null , 2)
+connection.on("initActivity", () => {
+    console.log("클릭했다.")
+    alert("커스텀엑티비틔를 설정하세요.")
 })
 
 connection.on("clickedNext", () => {
     console.log("완료했다.")
-    var configuration = JSON.parse(document.getElementById('configuration').value)
+    var configuration = alert("저장 되었습니다.")
     connection.trigger('updateActivity', configuration)
 })
